815d4468730b132e9ac7e2510437f77c1e30351b
AndreaOffredi <info@andreaoffredi.com>
2024-07-08 15:54:32 +0200

    ADD: price with sale

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
 // denominazione variabili
-let kmElement
-let etaElement
 let buttonElement
 let formElement
+let prezzoBiglietto
+let sconto
+let prezzoTotale
 
 // Richiamo variabili dal DOM e assegnazione
-kmElement = parseFloat (document.getElementById('km')) // number
-etaElement = parseFloat (document.getElementById('eta')) // number
 buttonElement = document.getElementById('calcola')
 formElement = document.getElementById('form-km')
 
-// togliamo l'evento Submit dal form
+// recupero input dal DOM
+const inputKmElement = document.getElementById('km')
+const inputEtaElement = document.getElementById('eta')
+
+
 formElement.addEventListener('submit', function (event){
+    // togliamo l'evento Submit dal form
     event.preventDefault()
-    console.log('submit', event)
+
+    // recuperiamo input km
+    const km = parseFloat (inputKmElement.value) //number
+    console.log(km)
+
+    // recuperiamo input eta
+    const eta = parseFloat (inputEtaElement.value) //number
+    console.log(eta)
+
+    // calcoliamo il prezzo del biglietto
+    prezzoBiglietto = km * 0.21
+
+    // calcoliamo lo sconto con una condizione
+    if (eta < 18) {
+        sconto = prezzoBiglietto * 0.20
+
+    } else if (eta > 65) {
+        sconto = prezzoBiglietto * 0.40
+    } else {
+        sconto = 0
+    }
+
+    // calcoliamo il prezzo del biglietto
+    prezzoTotale = prezzoBiglietto - sconto
 })
