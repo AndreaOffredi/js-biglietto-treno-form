30a3a3cfb6b5ada73c2517586ec2fcd8d8b17f70
AndreaOffredi <info@andreaoffredi.com>
2024-07-08 15:40:21 +0200

    ADD: css complete, JS submit event e creation var

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('ciao mamma')
+// denominazione variabili
+let kmElement
+let etaElement
+let buttonElement
+let formElement
+
+// Richiamo variabili dal DOM e assegnazione
+kmElement = parseFloat (document.getElementById('km')) // number
+etaElement = parseFloat (document.getElementById('eta')) // number
+buttonElement = document.getElementById('calcola')
+formElement = document.getElementById('form-km')
+
+// togliamo l'evento Submit dal form
+formElement.addEventListener('submit', function (event){
+    event.preventDefault()
+    console.log('submit', event)
+})
